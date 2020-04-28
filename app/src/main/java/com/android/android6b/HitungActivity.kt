f9a8c028a26974dfef9670b5e767ec6e0192820d
yusribjb <yusribjb@gmail.com>
2020-04-29 00:12:26 +0800

    Menghitung Vulume dan Validasinya

--- app/src/main/java/com/android/android6b/HitungActivity.kt
+++ app/src/main/java/com/android/android6b/HitungActivity.kt
@@ -2,6 +2,8 @@ package com.android.android6b
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hitung.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HitungActivity : AppCompatActivity() {
 
@@ -9,5 +11,34 @@ class HitungActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hitung)
 
+        buttonHitung.setOnClickListener{
+            var isEmptyField= false
+
+            val stringPanjang = editPanjang.text.toString() .trim()
+            val stringLebar = editLebar.text.toString() .trim()
+            val stringTinggi = editTinggi.text.toString() .trim()
+
+            if(stringPanjang.equals("")) {
+                isEmptyField = true
+                editPanjang.error = "Data Kosong"
+            }
+            if(stringLebar.isEmpty()) {
+                isEmptyField = true
+                editLebar.error = "Data Kosong"
+            }
+            if(stringTinggi == "") {
+                isEmptyField = true
+                editTinggi.error = "Data Kosong"
+            }
+
+            if(!isEmptyField) {
+
+                val panjang = stringPanjang.toFloat()
+                val lebar = stringLebar.toFloat()
+                val tinggi = stringTinggi.toFloat()
+
+                textHasil.text = (panjang * lebar * tinggi).toString()
+            }
+        }
     }
 }
--- app/src/main/java/com/android/android6b/MainActivity.kt
+++ app/src/main/java/com/android/android6b/MainActivity.kt
@@ -16,8 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView1 = findViewById<TextView>(R.id.textView1)
-        val button1 = findViewById<Button>(R.id.button1)
+        // val textView1 = findViewById<TextView>(R.id.textView1)
+        // val button1 = findViewById<Button>(R.id.button1)
 
         textView1.setOnClickListener {
             Toast.makeText(this, "hai", Toast.LENGTH_SHORT).show()
